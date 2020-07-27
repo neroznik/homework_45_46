5590e1564b2e4ba4cf61cd1594ad5f93b462319b
Evgenii <neroznik@yandex.ru>
2020-07-27 13:44:01 +0600

    Изменена модель ToDo_Tasks на Task

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,13 +2,13 @@ from django.utils import timezone
 from django.db import models
 
 
-class ToDo_Task(models.Model):
+class Tasks(models.Model):
     status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
 
     task = models.CharField(max_length=100, null=False, blank=False, verbose_name='Задача')
-    description = models.TextField(max_length=500, null=False, blank=True, verbose_name='Описание')
-    status = models.CharField (max_length=50, null=False,choices= status_choices, blank=False, default= 'new', verbose_name='Статус')
-    date_proccessing = models.DateField(default=timezone.now().strftime("%Y-%m-%d"), verbose_name='Дата выполнения', blank = False)
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание')
+    status = models.CharField (max_length=50, null=False,choices = status_choices, blank=False, default='new', verbose_name='Статус')
+    date_proccessing = models.DateField(default=timezone.now().strftime("%Y-%m-%d"), verbose_name='Дата выполнения', null=False, blank = False)
 
     class Meta:
         verbose_name = "Task"
