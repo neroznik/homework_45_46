b5b9a32ee6f83ae285fd53234e3daa20e8c38af1
Evgenii <neroznik@yandex.ru>
2020-07-30 18:20:00 +0600

    Добавленны HTML шаблоны

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, redirect, get_object_or_404
-from django.utils.timezone import make_naive
 
 from webapp.forms import TasksForm, BROWSER_DATETIME_FORMAT
 from webapp.models import Tasks, STATUS_CHOICES
@@ -10,11 +9,11 @@ def index_view(request):
     task = Tasks.objects.all()
     return render(request, 'index.html', context={'Tasks': task})
 
+
 def tasks_view(request, pk):
     task = get_object_or_404(Tasks, pk=pk)
-    context = {'Tasks': task }
+    context = {'Tasks': task}
     return render(request, 'task_view.html', context)
-
 
 
 def add_task(request):
@@ -23,15 +22,18 @@ def add_task(request):
     elif request.method == 'POST':
         form = TasksForm(data=request.POST)
         if form.is_valid():
-            task = Tasks.objects.create(**form.cleaned_data)
-            return redirect('task_view', pk=task.pk)
+            form = Tasks.objects.create(
+                task=form.cleaned_data['task'],
+                description=form.cleaned_data['description'],
+                status=form.cleaned_data['status'],
+                date_proccessing=form.cleaned_data['date_proccessing'])
+            return redirect('task_view', pk=form.pk)
         else:
             return render(request, 'add.html', context={
                 'form': form
             })
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
-
 
 
 def task_update_view(request, pk):
@@ -50,7 +52,10 @@ def task_update_view(request, pk):
     elif request.method == 'POST':
         form = TasksForm(data=request.POST)
         if form.is_valid():
-            Tasks.objects.filter(pk=pk).update(**form.cleaned_data)
+            Tasks.task = form.cleaned_data['task']
+            Tasks.description = form.cleaned_data['description']
+            Tasks.status = form.cleaned_data['status']
+            Tasks.date_proccessing = form.cleaned_data['date_proccessing']
             Tasks.save()
             return redirect('task_view', pk=Tasks.pk)
         else:
