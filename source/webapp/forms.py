a27ac25a39a0693e2a619ea787f4315d84838255
Evgenii <neroznik@yandex.ru>
2020-08-06 17:06:47 +0600

    Изменены модели и формы

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import STATUS_CHOICES
+from .models import Status, Types
 
-default_status = STATUS_CHOICES[0][0]
+default_status = Status.status['new']
+default_type = Types.types['task']
 
 
 BROWSER_DATETIME_FORMAT = '%Y-%m-%d'
@@ -10,5 +11,6 @@ BROWSER_DATETIME_FORMAT = '%Y-%m-%d'
 class TasksForm(forms.Form):
     task = forms.CharField(max_length=100, required=True, label='Задача')
     description = forms.CharField(max_length=1000, required=False, label='Описание', widget=forms.Textarea)
-    status = forms.ChoiceField(choices=STATUS_CHOICES, initial=default_status, label='Статус')
-    date_proccessing = forms.DateField(required=True, label='Дедлайн', widget=forms.DateInput(attrs={'type': 'date'}))
+    status = forms.ChoiceField(choices=Status.status, initial=default_status, label='Статус')
+    type = forms.ChoiceField(choices=Types.types, initial=default_type, label='Тип')
+    created_at = forms.DateField(required=True, label="Дедлайн", widget=forms.DateInput(attrs={'type': 'date'}))
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,15 +2,18 @@ from django.utils import timezone
 from django.db import models
 
 
+class Status(models.Model):
+    status = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
 
-STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
-
+class Types(models.Model):
+    types = [('task', 'Задача'), ('bug', 'Ошибка'), ('enhancement', 'Улучшение')]
 
 class Tasks(models.Model):
-    task = models.CharField(max_length=100, null=False, blank=False, verbose_name='Задача')
+    summary = models.CharField(max_length=100, verbose_name='Задача')
     description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание')
-    status = models.CharField (max_length=50, null=False,choices = STATUS_CHOICES, blank=False, default='new', verbose_name='Статус')
-    date_proccessing = models.DateField(default=timezone.now().strftime("%Y-%m-%d"), verbose_name='Дата выполнения', null=False, blank = False)
+    status = models.ForeignKey ('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey ('webapp.Types', related_name='types', on_delete=models.PROTECT, verbose_name= 'Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
     class Meta:
         verbose_name = "Task"
@@ -19,5 +22,7 @@ class Tasks(models.Model):
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.task)
+
+
 
 
